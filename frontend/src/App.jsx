9695ead1e5b7cef8f88213817934ca45fe72b2a3
Lê Anh Vũ <leanhvu>
2021-06-09 14:57:43 +0700

    lav update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,22 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // pages
-// import Search from "./Components/MyRecipes/Search";
-// import RecipeList from "./Components/MyRecipes/RecipeList";
-// import Pagination from "./Components/MyRecipes/Pagination";
-// import Header from "./Components/Header/Header";
 
-import DetailRecipe from "./Components/Detail/DetailRecipe";
-import Home from "./Components/Home/Home";
+// import Home from "./Components/Home/Home";
 import Login from "./Components/Login";
 import AddRecipe from "./Components/AddRecipe/AddRecipe";
-import MyRecipes from "./Components/MyRecipes/RecipeList";
+// import MyRecipes from "./Components/MyRecipes/RecipeList";
+// import DetailRecipe from "./Components/Detail/DetailRecipe";
 
 
 // contexts
 import { AuthContext } from './Contexts/AuthContext';
 // custom hook
+
 import useFetch from './hooks/useFetch'
 import Nav from "./Components/Header/Nav";
-import CreateAccount from "./Components/Login/CreateAccount";
+// import CreateAccount from "./Components/Login/CreateAccount";
+
 function App() {
   const [auth, setAuth] = useContext(AuthContext);
   const [{ response, error, isLoading }, doFetch] = useFetch("https://jsonplaceholder.typicode.com/todos/");
@@ -30,17 +28,17 @@ function App() {
 
       <Nav />
       <Switch>
-        <Route exact path="/"> <Home /> </Route>
+        {/* <Route exact path="/"> <Home /> </Route> */}
 
-        <Route path="/my-recipes"><MyRecipes /></Route>
+        {/* <Route path="/my-recipes"><MyRecipes /></Route> */}
 
         <Route path="/add-recipe"><AddRecipe /></Route>
 
-        <Route path="/detail-recipe"><DetailRecipe /></Route>
+        {/* <Route path="/detail-recipe"><DetailRecipe /></Route> */}
 
         <Route path="/login"><Login /></Route>
 
-        <Route path="/register"><CreateAccount /></Route>
+        {/* <Route path="/register"><CreateAccount /></Route> */}
       </Switch>
     </Router>
   );
--- frontend/src/components/AddRecipe/AddRecipe.jsx
+++ frontend/src/components/AddRecipe/AddRecipe.jsx
@@ -1,92 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import './create-recipe.css';
 
+
+
 export default function AddRecipe() {
-  const [name, setName] = useState('');
-  const [image, setImage] = useState('https://img.taste.com.au/z09DD4Ls/taste/2018/07/zucchini-lasagne-roll-ups-139165-1.jpg');
-  const [description, setDescription] = useState('')
+  const [newRecipe, setNewRecipe] = useState({
+    name: "",
+    image: "https://img.taste.com.au/z09DD4Ls/taste/2018/07/zucchini-lasagne-roll-ups-139165-1.jpg",
+    description: "",
+    category: "1",
+    ingredients: [],
+    steps: [],
+  });
 
-  const [category, setCategory] = useState(0);
-  const [ingredient, setIngredient] = useState('');
-  const [step, setStep] = useState('');
+  // DB values
+  const [newCategory, setNewCategory] = useState('');
+  const [categoriesFromDB, setCategoriesFromDB] = useState(['one', 'two', 'three']);
+  const handleAddNewCategory = () => {
+    // update database
 
-  // save list value
-  const [categories, setCategories] = useState(['one', 'two', 'three']);
-  const [ingredients, setIngredients] = useState([]);
-  const [steps, setSteps] = useState([]);
-
-  const [errorDialog, setErrorDialog] = useState({ check: false, title: '', content: '' });
-
-
-  const handleClickAddNewIngredient = () => {
-    if (ingredient === '') return;
-    setIngredients([...ingredients, ingredient]);
+    // update categories
+    setCategoriesFromDB([...categoriesFromDB, newCategory]);
   }
-  const handleClickAddNewCategory = () => {
-    if (category === '') return;
-    setCategories([...categories, category]);
+
+  const [newIngredient, setNewIngredient] = useState('');
+  const [tempIngredients, setTempIngredients] = useState([]);
+  const handleAddNewIngredient = () => {
+    setTempIngredients([...tempIngredients, newIngredient]);
   }
-  const handleClickAddNewStep = () => {
-    if (step === '') return;
-    setSteps([...steps, step]);
+  const [newStep, setNewStep] = useState('');
+  const [tempSteps, setTempSteps] = useState([]);
+  const handleAddNewStep = () => {
+    setTempSteps([...tempSteps, newStep]);
   }
   const handleSubmit = (e) => {
     e.preventDefault();
-
-
-    console.log('nameRecipe: ', name);
-    console.log('imageRecipe: ', image);
-    console.log('description: ', description);
-    console.log('category: ', category);
-    // console.log('ingredient: ', ingredient);
-    // console.log('step: ', step);
-
-    console.log(ingredients)
-    console.log(steps)
-
-    // setErrorDialog] = useState({check: false, title: '', content: ''});
-    if (name === '') { setErrorDialog({ check: true, title: 'Error name input', content: 'Name is empty' }); return; }
-    if (image === '') { setErrorDialog({ check: true, title: 'Error image input', content: 'Image is empty' }); return; }
-    if (description === '') { setErrorDialog({ check: true, title: 'Error description input', content: 'Description is empty' }); return; }
-    if (category === '') { setErrorDialog({ check: true, title: 'Error category input', content: 'Category is empty' }); return; }
-    if (ingredients.length === 0) { setErrorDialog({ check: true, title: 'Error ingredients input', content: 'Ingredients is empty' }); return; }
-    if (steps.length === 0) { setErrorDialog({ check: true, title: 'Error steps input', content: 'Steps is empty' }); return; }
-  }
-
-  // CHƯA LÀM XONG.
-  const bodyStyle = (check) => {
-    if (check === true) {
-      document.body.style.overflow = 'hidden'
-      
-    } else {
-      document.body.style.overflow = 'unset';
-    }
-  }
-  
-  const ErrorMessage = (props) => {
-    return (
-      <div className="error__dialog-wrapper">
-        <div className="error__dialog">
-          <div className="error__dialog-title">
-            {props.title}
-          </div>
-          <div className="error__dialog-content">
-            {props.content}
-          </div>
-          <div className="error__dialog-footer">
-            <button onClick={(e) => setErrorDialog({ ...errorDialog, check: false })} type="button" className="btn btn-secondary px-3 mt-2">OK</button>
-          </div>
-        </div>
-      </div>
-    );
+    setNewRecipe({ ...newRecipe, ingredients: [...tempIngredients], steps: [...tempSteps] });
+    console.log(newRecipe);
   }
   return (
-
     <div className="body-add-recipe">
-
-      { bodyStyle(errorDialog.check)}
-      {errorDialog.check && ErrorMessage(errorDialog)}
-
       <div className="container">
         <h2 className="text-center my-4">CREATE NEW RECIPE</h2>
       </div>
@@ -96,7 +49,7 @@ export default function AddRecipe() {
           {/* name */}
           <div className="input-group mb-3">
             <span className="input-group-text">Recipe name</span>
-            <input onChange={(e) => setName(e.target.value)} autoFocus type="text" className="form-control" placeholder="Recipe name" aria-label="recipe-name" aria-describedby="lav" />
+            <input onChange={e => setNewRecipe({ ...newRecipe, name: e.target.value })} autoFocus type="text" className="form-control" placeholder="Recipe name" aria-label="recipe-name" aria-describedby="lav" />
           </div>
 
           {/* image | description */}
@@ -104,13 +57,13 @@ export default function AddRecipe() {
             <div className="col-md-4 col-sm-6">
               <div className="input-group">
                 <label>
-                  <input onChange={(e) => setImage(e.target.value)} type="file" className="form-control" />
-                  <img style={{ cursor: 'pointer' }} src={image} alt="image food" className="img-fluid img-thumbnail" />
+                  <input onChange={e => setNewRecipe({ ...newRecipe, image: e.target.value })} type="file" className="form-control" />
+                  <img style={{ cursor: 'pointer' }} alt="image food" className="img-fluid img-thumbnail" />
                 </label>
               </div>
             </div>
             <div className="col-md-8 col-sm-6">
-              <textarea onChange={(e) => setDescription(e.target.value)} style={{ height: '100%' }} className="form-control" placeholder="Enter your description here" defaultValue={""} />
+              <textarea onChange={e => setNewRecipe({ ...newRecipe, description: e.target.value })} style={{ height: '100%' }} className="form-control" placeholder="Enter your description here" defaultValue={""} />
             </div>
           </div>
 
@@ -120,16 +73,14 @@ export default function AddRecipe() {
               <h5 className="text-center">Categories</h5>
 
               <div className="input-group">
-                <select onChange={(e) => setCategory(e.target.value)} className="form-select" style={{ maxWidth: '36%' }}>
-                  {categories.map((item, index) => {
-                    if (item === category) {
-                      setCategory(index); // set category_id. 
-                      return (<option selected key={index} value={index}>{item}</option>);
-                    } else return (<option key={index} value={index}>{item}</option>);
-                  })}
+                <select onChange={e => setNewRecipe({ ...newRecipe, category: e.target.value })} className="form-select" style={{ maxWidth: '36%' }}>
+                  {
+                    categoriesFromDB.map((item, index) =>
+                      <option key={index} value={index}>{item}</option>)
+                  }
                 </select>
-                <input onChange={(e) => setCategory(e.target.value)} type="text" className="form-control" placeholder="Add new category" />
-                <button onClick={handleClickAddNewCategory} className="btn mx-1 btn-success" type="button">Add new</button>
+                <input defaultValue={newCategory} onChange={e => setNewCategory(e.target.value)} type="text" className="form-control" placeholder="Add new category" />
+                <button onClick={handleAddNewCategory} className="btn mx-1 btn-success" type="button">Add new</button>
               </div>
             </div>
 
@@ -140,30 +91,31 @@ export default function AddRecipe() {
             <div className="col-lg-6 col-md-6 col-sm-12">
               <h5 className="text-center">Ingredients</h5>
               <div className="input-group mb-3">
-                <textarea onChange={(e) => setIngredient(e.target.value)} className="form-control" aria-label="With textarea" defaultValue={""} />
-                <button onClick={handleClickAddNewIngredient} className="btn mx-1 btn-success" type="button" >Add new</button>
+                <textarea onChange={e => setNewIngredient(e.target.value)} className="form-control" aria-label="With textarea" defaultValue={""} />
+                <button onClick={handleAddNewIngredient} className="btn mx-1 btn-success" type="button" >Add new</button>
               </div>
               {/* hiển thị ingredients */}
               <ol className="list-group list-group-numbered">
-                {ingredients.map((ingredient_content, index) => {
-                  return (<li key={index} className="list-group-item">{ingredient_content}</li>)
-                })}
+                {
+                  tempIngredients.map((content, index) =>
+                    <li key={index} className="list-group-item">{content}</li>)
+                }
               </ol>
             </div>
 
             <div className="col-lg-6 col-md-6 col-sm-12">
               <h5 className="text-center">Steps</h5>
-              <div className="input-group">
+              <div className="input-group mb-3">
                 {/* <span className="input-group-text">Step</span> */}
-                <textarea onChange={(e) => setStep(e.target.value)} className="form-control" aria-label="With textarea" defaultValue={""} />
-                <button onClick={handleClickAddNewStep} className="btn mx-1 btn-success" type="button">Add new</button>
+                <textarea onChange={e => setNewStep(e.target.value)} className="form-control" aria-label="With textarea" defaultValue={""} />
+                <button onClick={handleAddNewStep} className="btn mx-1 btn-success" type="button">Add new</button>
               </div>
 
               {/* hiển thị steps */}
               <ol className="list-group list-group-numbered">
-                {steps.map((step_content, index) => {
-                  return (<li key={index} className="list-group-item">{step_content + ""}</li>)
-                })}
+                {tempSteps.map((content, index) =>
+                  <li key={index} className="list-group-item">{content}</li>)
+                }
               </ol>
 
             </div>
