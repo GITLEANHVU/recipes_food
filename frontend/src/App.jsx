d4b1771b5da19475bca450e217ed91a785e189ac
TranThiHoaiTrang <68778348+TranThiHoaiTrang@users.noreply.github.com>
2021-06-10 19:19:05 +0700

    update layout and api Comment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,19 @@
 import React, { Fragment, useContext, useEffect } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // pages
-// import Search from "./Components/MyRecipes/Search";
-// import RecipeList from "./Components/MyRecipes/RecipeList";
-// import Pagination from "./Components/MyRecipes/Pagination";
-// import Header from "./Components/Header/Header";
 
 import DetailRecipe from "./Components/Detail/DetailRecipe";
 import Home from "./Components/Home/Home";
-import Login from "./Components/Login";
+// import Login from "./Components/Login";
 import AddRecipe from "./Components/AddRecipe/AddRecipe";
 import MyRecipes from "./Components/MyRecipes/RecipeList";
-
+import Nav from "./Components/Header/Nav";
+// import CreateAccount from "./Components/Login/CreateAccount";
 
 // contexts
 import { AuthContext } from './Contexts/AuthContext';
 // custom hook
 import useFetch from './hooks/useFetch'
-import Nav from "./Components/Header/Nav";
-import CreateAccount from "./Components/Login/CreateAccount";
 function App() {
   const [auth, setAuth] = useContext(AuthContext);
   const [{ response, error, isLoading }, doFetch] = useFetch("https://jsonplaceholder.typicode.com/todos/");
@@ -38,9 +33,9 @@ function App() {
 
         <Route path="/detail-recipe"><DetailRecipe /></Route>
 
-        <Route path="/login"><Login /></Route>
+        {/* <Route path="/login"><Login /></Route> */}
 
-        <Route path="/register"><CreateAccount /></Route>
+        {/* <Route path="/register"><CreateAccount /></Route> */}
       </Switch>
     </Router>
   );
