342b4bb2b26f9efba1367706051000609a09363c
GITThaiThiMyTien <thaitiendrm@gmail.com>
2021-06-11 17:05:41 +0700

    tien

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,23 @@ import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 // pages
 
+<<<<<<< HEAD
 import Home from "./Components/Home/Home";
 import Login from "./Components/Login/Login";
 // import AddRecipe from "./Components/AddRecipe/AddRecipe";
 import MyRecipes from "./Components/MyRecipes/MyRecipe";
+=======
+<<<<<<< HEAD
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login";
+import AddRecipe from "./Components/AddRecipe/AddRecipe";
+=======
+// import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+// import AddRecipe from "./Components/AddRecipe/AddRecipe";
+>>>>>>> c4c768fee1079cac7c3134109f65bd619bf800ab
+// import MyRecipes from "./Components/MyRecipes/RecipeList";
+>>>>>>> ff4dda524b8fa7214f0079d1b85ea0b6ddcf94d4
 // import DetailRecipe from "./Components/Detail/DetailRecipe";
 
 // contexts
@@ -24,26 +37,49 @@ function App() {
   // console.log("App: ", response, error, isLoading)
 
   return (
+<<<<<<< HEAD
+    
+    <Router>
+      <Nav />
+      <UploadFiles />
+      <Switch>
+        <Route exact path="/"> <Home /> </Route>
+=======
   
      
       <Router>
         <Nav />
         {/* <UploadFiles /> */}
         <Switch>
+<<<<<<< HEAD
           <Route exact path="/"> <Home /> </Route>
+=======
+          {/* <Route exact path="/"> <Home /> </Route> */}
+>>>>>>> c4c768fee1079cac7c3134109f65bd619bf800ab
+>>>>>>> ff4dda524b8fa7214f0079d1b85ea0b6ddcf94d4
 
           <Route path="/my-recipes"><MyRecipes /></Route>
 
+<<<<<<< HEAD
+        <Route path="/add-recipe"><AddRecipe /></Route>
+=======
           {/* <Route path="/add-recipe"><AddRecipe /></Route> */}
+>>>>>>> c4c768fee1079cac7c3134109f65bd619bf800ab
 
           {/* <Route path="/detail-recipe"><DetailRecipe /></Route> */}
 
           <Route path="/login"><Login /></Route>
 
+<<<<<<< HEAD
+        {/* <Route path="/register"><CreateAccount /></Route> */}
+      </Switch>
+    </Router>
+=======
           <Route path="/register"> <CreateAccount /> </Route>
         </Switch>
       </Router>
       
+>>>>>>> c4c768fee1079cac7c3134109f65bd619bf800ab
   );
 }
 
@@ -64,7 +100,7 @@ function UploadFiles() {
     const formData = new FormData();
     formData.append('sendimage', files[0]);
     // console.log(formData)
-    fetch(`http://localhost/recipes_food/backend/api/upload.php`, {
+    fetch(`http://localhost:83/recipes_food/backend/api/upload.php`, {
       mode: "no-cors",
       method: "POST",
       body: formData
