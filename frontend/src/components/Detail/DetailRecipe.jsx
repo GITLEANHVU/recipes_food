f729f16b95453e6e0ea003fd074cb48a102af3dd
Nguyen Ngoc Truong <ngoctruong472k@gmail.com>
2021-06-13 14:25:18 +0700

    commit update DetailRecipe (comment, click heart) - Truong

--- frontend/src/components/Detail/DetailRecipe.jsx
+++ frontend/src/components/Detail/DetailRecipe.jsx
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
 import './DetailRecipe.css';
 
 export default function DetailRecipe() {
-    const changeHeart = () => {
-        document.querySelector('.fa-heart').style.color = 'red';
-    }
+    
     const [comment, setComment] = useState('');
-    const [comments, setComments] = useState([]);
+    const [colorHeart, setColorHeart] = useState('black');
+    
+    // mỗi thằng chứa 2 time riêng
+    const [comments, setComments] = useState([
+        { content: "", craeted_at: "", updated_at: "" }
+    ]);
     const [step, setStep] = useState('');
     const [ingredient, setIngredient] = useState('');
     //
@@ -19,28 +23,33 @@ export default function DetailRecipe() {
     ]);
     const clickAddNewComment = () => {
         if (comment === '') return;
-        setComments([...comments, comment]);
+        setComments([...comments, { content: comment, craeted_at: String(formatDate()), updated_at: "" }]);
     }
-    //dang loi
-    function dateComment() {
-        // Cái quan trọng là ngày tháng thì không lấy
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-        return time;
-    };
+
+    function formatDate() {
+        var date = new Date();
+        var hours = date.getHours();
+        var minutes = date.getMinutes();
+        var day = date.getDay();
+        var ampm = hours >= 12 ? 'pm' : 'am';
+        hours = hours % 12;
+        hours = hours ? hours : 12;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        var strTime = day + hours + ':' + minutes + ' ' + ampm;
+        return ('0' + (date.getMonth() + 1)).slice(-2) + "/" + ('0' + date.getDate()).slice(-2) + "/" + date.getFullYear() + "  " + strTime;
+    }
     return (
         <div className="detailRecipe">
             <div className="container">
                 <div className="row g-2 contentTop">
                     <div className="col-4 sm-3">
                         <div className="imgRecipe">
-                            <img className="img-detail" src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" />
+                            <img className="img-detail" src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" alt="" />
                         </div>
                         <div className="status">
-                            <a href="#" className="btn heart" onClick={changeHeart}><i className="fas fa-heart"></i></a>
-                            <a href="#" className="btn editRecipe"><i className="far fa-edit"></i></a>
-                            <a href="#" className="btn deleteRecipe"><i className="fas fa-trash"></i></a>
-
+                            <a href="#/" className="btn heart"><i className="fas fa-heart" style={{color:colorHeart}} onClick={()=>{setColorHeart("red")}}></i></a>
+                            <a href="#/" className="btn editRecipe"><i className="far fa-edit"></i></a>
+                            <a href="#/" className="btn deleteRecipe"><i className="fas fa-trash"></i></a>
                         </div>
                     </div>
                     <div className="col-8 sm-9">
@@ -80,10 +89,10 @@ export default function DetailRecipe() {
                     </div>
                 </div>
                 <div className="bod">
-                    <div className="comment">(0) Bình Luận</div>
+                    <div className="comment">Bình Luận</div>
                     <div className="SortComment">
                         Sắp Xếp
-                    <select className="selectSort">
+                        <select className="selectSort">
                             <option>Mới nhất</option>
                             <option>Cũ nhất</option>
                         </select>
@@ -93,7 +102,7 @@ export default function DetailRecipe() {
                     <div className="col-4">
                         <div className="form-comment">
                             <div className="avatar">
-                                <img src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" width="50px" height="50px" />
+                                <img src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" width="50px" height="50px" alt="" />
                             </div>
                             <div className="cmt">
                                 <div className="form-floating">
@@ -105,21 +114,28 @@ export default function DetailRecipe() {
                         </div>
                     </div>
                 </div>
-                {comments.map((comment_content, index) => {
-                    return (
-                        <div className="form-comment" key={index}>
-                            <div className="avatar">
-                                <img src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" width="50px" height="50px" />
-                            </div>
-                            <div className="contentAcc">
-                                <p className="nameAcc">Name</p>
-                                <p className="content">{comment_content}</p>
-                                {/* Lỗi date chưa fix dc */}
-                                <p className="dateComment">{dateComment()}</p>
-                            </div>
-                        </div>
-                    )
-                })}
+                <div className="row g-2">
+                    <div className="col-12 col-md-5 dateRight">
+                        {comments.map((comment_content, index) => {
+                            if (comment_content.content !== "") {
+                                return (
+                                    <div className="form-comment" key={index}>
+                                        <div className="avatar">
+                                            <img src="https://image.cooky.vn/posproduct/g0/5075/s400x400/93644f58-2233-456c-b6f2-f670491e9f65.jpeg" width="50px" height="50px" alt="" />
+                                        </div>
+                                        <div className="contentAcc">
+                                            <div className="nameDate">
+                                                <p className="nameAcc">Name {console.log(index)}</p>
+                                                <p className="dateComment"><i className="far fa-clock"></i> {comment_content.craeted_at}</p>
+                                            </div>
+                                            <p className="content">{comment_content.content}</p>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        })}
+                    </div>
+                </div>
             </div>
             <p></p>
         </div>
