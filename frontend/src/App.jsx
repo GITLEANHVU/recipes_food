dcd072791834395bb1d6d411f62592fd096bffe8
Lê Anh Vũ <leanhvu>
2021-06-13 10:12:00 +0700

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,79 +1,55 @@
 import React, { useContext, useEffect, useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-// pages
 
-import Home from "./Components/Home/Home";
-import Login from "./Components/Login";
-import AddRecipe from "./Components/AddRecipe/AddRecipe";
-// import MyRecipes from "./Components/MyRecipes/RecipeList";
+// Components
 // import DetailRecipe from "./Components/Detail/DetailRecipe";
+import Nav from "./Components/Header/Nav";
+import Home from "./Components/Home/Home";
+// import MyRecipes from "./Components/MyRecipes/RecipeList";
+import AddRecipe from "./Components/AddRecipe/AddRecipe";
+import Login from "./Components/Login";
+// import CreateAccount from "./Components/Login/CreateAccount";
 
 // contexts
 import { AuthContext } from './Contexts/AuthContext';
-// custom hook
 
-import useFetch from './hooks/useFetch'
-import Nav from "./Components/Header/Nav";
-import CreateAccount from "./Components/Login/CreateAccount";
-// import CreateAccount from "./Components/Login/CreateAccount";
+// custom hook
+// import useFetch from './hooks/useFetch'
 
 function App() {
   // const [auth, setAuth] = useContext(AuthContext);
   // const [{ response, error, isLoading }, doFetch] = useFetch("https://jsonplaceholder.typicode.com/todos/");
   // useEffect(() => { doFetch(); }, [doFetch]);
   // console.log("App: ", response, error, isLoading)
-
   return (
-    
-    <Router>
-      <Nav />
-      <UploadFiles />
-      <Switch>
-        <Route exact path="/"> <Home /> </Route>
+    <React.Fragment>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          {/* <Route path="/my-recipes">
+          <MyRecipes />
+          </Route> */}
+          <Route path="/add-recipe">
+            <AddRecipe />
+          </Route>
+          {/* <Route path="/detail-recipe">
+          <DetailRecipe />
+          </Route> */}
+          <Route path="/login">
+            <Login />
+          </Route>
 
-          {/* <Route path="/my-recipes"><MyRecipes /></Route> */}
-
-        <Route path="/add-recipe"><AddRecipe /></Route>
-
-          {/* <Route path="/detail-recipe"><DetailRecipe /></Route> */}
-
-          <Route path="/login"><Login /></Route>
-
-        {/* <Route path="/register"><CreateAccount /></Route> */}
-      </Switch>
-    </Router>
+          {/* <Route path="/register">
+        <CreateAccount />
+        </Route> */}
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
 export default App;
 
-
-// const Main = () => {
-//   return auth.isAuth ?
-//     "Da dang nhap"
-//     :
-//     "Chua dang nhap";
-// }
-
-
-function UploadFiles() {
-  const handleUploadFile = e => {
-    const files = e.target.files;
-    const formData = new FormData();
-    formData.append('sendimage', files[0]);
-    // console.log(formData)
-    fetch(`http://localhost:83/recipes_food/backend/api/upload.php`, {
-      mode: "no-cors",
-      method: "POST",
-      body: formData
-    })
-      .then(res => res)
-      .then(result => console.log(result));
-  }
-  return (
-    <form>
-      <h1>Upload file</h1>
-      <input onChange={e => handleUploadFile(e)} type="file" name="sendimage" />
-    </form>
-  );
-}
