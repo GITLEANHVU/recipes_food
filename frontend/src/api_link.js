dd602445c0ab3998da6faef7ccde5f3547b30006
Nguyen Ngoc Truong <ngoctruong472k@gmail.com>
2021-06-22 06:53:26 +0700

    Update Detail steps

--- frontend/src/api_link.js
+++ frontend/src/api_link.js
@@ -1,17 +1,5 @@
 // Link Folder image
-<<<<<<< HEAD
-export const REACT_APP_UPLOADS = "http://localhost/recipes_food/backend/uploads/images"
-export const REACT_APP_UPLOADS_CAROUSEL = "http://localhost/recipes_food/backend/uploads/carousel_img"
-
-// Link Account
-export const API_LINK_ACCOUNT_DELETE = "http://localhost/recipes_food/backend/api/Account/delete.php"
-export const API_LINK_ACCOUNT_UPDATE = "http://localhost/recipes_food/backend/api/Account/update.php"
-export const API_LINK_ACCOUNT_CREATE = "http://localhost/recipes_food/backend/api/Account/create.php"
-export const API_LINK_ACCOUNT_BY_ID = "http://localhost/recipes_food/backend/api/Account/get_account_by_id.php"
-export const API_LINK_ACCOUNT_READ_SINGLE = "http://localhost/recipes_food/backend/api/Account/read_single.php"
-=======
 export const REACT_APP_UPLOADS = "http://localhost:82/recipes_food/backend/uploads/images"
-export const REACT_APP_UPLOADS_RECIPE = "http://localhost:82/recipes_food/backend/uploads/recipes"
 export const REACT_APP_UPLOADS_CAROUSEL = "http://localhost:82/recipes_food/backend/uploads/carousel_img"
 export const REACT_APP_UPLOADS_USER = "http://localhost:82/recipes_food/backend/uploads/user_img"
 
@@ -21,7 +9,6 @@ export const API_LINK_ACCOUNT_UPDATE = "http://localhost:82/recipes_food/backend
 export const API_LINK_ACCOUNT_CREATE = "http://localhost:82/recipes_food/backend/api/Account/create.php"
 export const API_LINK_ACCOUNT_BY_ID = "http://localhost:82/recipes_food/backend/api/Account/get_account_by_id.php"
 export const API_LINK_ACCOUNT_READ_SINGLE = "http://localhost:82/recipes_food/backend/api/Account/read_single.php"
->>>>>>> 7180c4cc120b4fce8015a72e65141b50e5d11409
 
 // Link Recipe
 export const API_LINK_RECIPE_CREATE = "http://localhost:82/recipes_food/backend/api/Recipe/create.php"
@@ -36,21 +23,6 @@ export const API_LINK_RECIPE_RECIPE_BY_NAME = "http://localhost:82/recipes_food/
 export const API_LINK_RECIPE_READ_ACCOUNT_NAME_BY_NAME = "http://localhost:82/recipes_food/backend/api/Recipe/read_by_Name.php"
 
 // Link Category
-<<<<<<< HEAD
-export const API_LINK_CATEGORY_READ = "http://localhost/recipes_food/backend/api/Category/read.php"
-export const API_LINK_CATEGORY_CREATE = "http://localhost/recipes_food/backend/api/Category/create.php"
-export const API_LINK_CATEGORY_DELETE = "http://localhost/recipes_food/backend/api/Category/delete.php"
-export const API_LINK_CATEGORY_UPDATE = "http://localhost/recipes_food/backend/api/Category/update.php"
-export const API_LINK_CATEGORY_READ_SINGLE = "http://localhost/recipes_food/backend/api/Category/read_single.php"
-export const API_LINK_CATEGORY_READ_ALL = "http://localhost/recipes_food/backend/api/Category/read.php"
-
-// Link Comment
-export const API_LINK_COMMENT_READ = "http://localhost/recipes_food/backend/api/Comment/read.php"
-export const API_LINK_COMMENT_CREATE = "http://localhost/recipes_food/backend/api/Comment/create.php"
-export const API_LINK_COMMENT_DELETE = "http://localhost/recipes_food/backend/api/Comment/delete.php"
-export const API_LINK_COMMENT_UPDATE = "http://localhost/recipes_food/backend/api/Comment/update.php"
-export const API_LINK_COMMENT_READ_SINGLE = "http://localhost/recipes_food/backend/api/Comment/read_single.php"
-=======
 export const API_LINK_CATEGORY_READ = "http://localhost:82/recipes_food/backend/api/Category/read.php"
 export const API_LINK_CATEGORY_CREATE = "http://localhost:82/recipes_food/backend/api/Category/create.php"
 export const API_LINK_CATEGORY_DELETE = "http://localhost:82/recipes_food/backend/api/Category/delete.php"
@@ -65,4 +37,3 @@ export const API_LINK_COMMENT_DELETE = "http://localhost:82/recipes_food/backend
 export const API_LINK_COMMENT_UPDATE = "http://localhost:82/recipes_food/backend/api/Comment/update.php"
 export const API_LINK_COMMENT_READ_SINGLE = "http://localhost:82/recipes_food/backend/api/Comment/read_single.php"
 export const API_LINK_SORT_COMMENT = "http://localhost:82/recipes_food/backend/api/Comment/sort_by_created_at.php"
->>>>>>> 7180c4cc120b4fce8015a72e65141b50e5d11409
