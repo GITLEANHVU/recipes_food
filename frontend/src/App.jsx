7a4d2a56c723d349326f99ee4a23d37084dec273
leanhvu21042001@gmail.com <leanhvu1412>
2021-11-22 11:09:57 +0700

    Organize source tree

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,64 +1,11 @@
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// Components
-import Home from "./components/Home/Home";
-import MyRecipe from "./components/MyRecipes/MyRecipe";
-import AU_Recipe from "./components/AddRecipe/AU_Recipe";
-import Login from "./components/Login/Login";
-import CreateAccount from "./components/Login/CreateAccount";
-import NotFound from "./components/Error/NotFound";
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import DetailRecipe from "./components/Detail/DetailRecipe";
 
 function App() {
   return (
     <React.Fragment>
       <Router>
-
-        {/* header */}
-        <Header />
-        <Switch>
-
-          {/* Home page */}
-          <Route exact path="/">
-            <Home />
-          </Route>
-
-          {/* My recipe */}
-          <Route path="/my-recipes">
-            <MyRecipe />
-          </Route>
-
-          {/* Add recipe */}
-          <Route exact path="/au-recipe">
-            {/* <AddRecipe /> */}
-            <AU_Recipe />
-          </Route>
-          {/* Update recipe */}
-          <Route path="/au-recipe/:id">
-            <AU_Recipe />
-          </Route>
-
-          {/* Detail recipe */}
-          <Route path="/detail-recipe/:id">
-            <DetailRecipe />
-          </Route>
-
-          {/* Login */}
-          <Route path="/login">
-            <Login />
-          </Route>
-          {/* register */}
-          <Route path="/register">
-            <CreateAccount />
-          </Route>
-
-          <Route path='*' exact={true} component={NotFound} />
-
-        </Switch>
-        <Footer />
+        <Switch></Switch>
       </Router>
     </React.Fragment>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { AuthContextProvider } from "./Contexts/AuthContext";
 
 ReactDOM.render(
-  <AuthContextProvider>
-    <App />
-  </AuthContextProvider>,
+  <App />,
   document.getElementById('root')
 );
